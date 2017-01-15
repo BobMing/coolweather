bb90bdbf36e51b32cd1b75c7f206a76a70d1a6dc
BobMing <bobmingxie@vip.qq.com>
2017-01-16 00:21:51 +0800

    complete the function of traversing 3-level lists

---
+++ app/src/main/java/com/coolweather/app/util/HttpCallbackListener.java
@@ -0,0 +1,10 @@
+package com.coolweather.app.util;
+
+/**
+ * Created by Admin on 2017/1/15 0015.
+ */
+
+public interface HttpCallbackListener {
+    void onFinish(String response);
+    void onError(Exception e);
+}
