1f3872f0c6b735aa9b830c3a29c236e9d35ba771
BobMing <bobmingxie@vip.qq.com>
2017-01-30 16:00:32 +0800

    add the function of automatically update weather info

--- app/src/main/java/com/coolweather/app/WeatherActivity.java
+++ app/src/main/java/com/coolweather/app/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.coolweather.app;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -9,6 +10,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.coolweather.app.gson.Forecast;
 import com.coolweather.app.gson.Weather;
+import com.coolweather.app.service.AutoUpdateService;
 import com.coolweather.app.util.HttpUtil;
 import com.coolweather.app.util.Utillity;
 
@@ -217,5 +220,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
+        Log.w("startService: ", "AutoUpdateService");
     }
 }
